60fbd19abf0f79c97050fb1927fc977cf73f9566
Jules Aguillon <juloo.dsi@gmail.com>
2020-10-04 16:50:24 +0200

    Vim: Implement local vimrc by wrapping Vim
    Adding a '.vimrc' symlink doesn't always work because Vim ignores some
    commands for security reasons.
    
    Instead, wrap 'vim' to add the CLI arguments to source the local vimrc.
    
    The wrapper is a bit complicated because it cannot point to a specific
    package to avoid shadowing an existing override.

--- modules/vim.nix
+++ modules/vim.nix
@@ -2,23 +2,36 @@
 
 with lib;
 
+let
+  # Wrap vim to source the local vimrc
+  make_wrapped_vim = vimrc:
+    let vimrc_file = builtins.toFile "${config.name}-vimrc" config.vimrc; in
+    # Expect vim to be in the user's environment on purpose,
+    # to avoid shadowing an existing override/wrap
+    pkgs.runCommand "${config.name}-vim" {} ''
+      mkdir -p "$out/bin"
+      cat <<EOF > "$out/bin/vim"
+      #!/usr/bin/env bash
+      this="$out"
+      PATH=\''${PATH//\$this/} # Remove this package from the PATH
+      exec vim -c "source ${vimrc_file}" "\$@"
+      EOF
+      chmod +x "$out/bin/vim"
+    '';
+in
+
 {
   options = {
     vimrc = mkOption {
       type = types.lines;
       default = "";
-      description = "Local vimrc. Will be symlinked at the root of the workspace.";
+      description = "Local vimrc.";
     };
 
   };
 
   config = mkIf (config.vimrc != "") {
-    activation_script = ''
-      ln -sf "${builtins.toFile "${config.name}-vimrc" config.vimrc}" .vimrc
-    '';
-    git.gitignore = ''
-      /.vimrc
-    '';
+    buildInputs = [ (make_wrapped_vim config.vimrc) ];
 
   };
 }
