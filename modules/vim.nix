075fd24827b550a513d76f2611865ebc1d4b7dd7
Jules Aguillon <juloo.dsi@gmail.com>
2020-12-20 20:56:11 +0100

    Vim: Fix wrapper never returning
    $out was not always the final path, $0 is better in most cases

--- modules/vim.nix
+++ modules/vim.nix
@@ -8,15 +8,13 @@ let
     let vimrc_file = builtins.toFile "${config.name}-vimrc" config.vimrc; in
     # Expect vim to be in the user's environment on purpose,
     # to avoid shadowing an existing override/wrap
-    pkgs.runCommand "${config.name}-vim" {} ''
-      mkdir -p "$out/bin"
-      cat <<EOF > "$out/bin/vim"
-      #!/usr/bin/env bash
-      this="$out"
-      PATH=\''${PATH//\$this/} # Remove this package from the PATH
-      exec vim -c "source ${vimrc_file}" "\$@"
-      EOF
-      chmod +x "$out/bin/vim"
+    pkgs.writeShellScriptBin "vim" ''
+      this=''${0%/vim}
+      p=:$PATH:
+      p=''${p//:$this:/}
+      p=''${p%:}
+      export PATH=''${p#:}
+      exec vim -c "source ${vimrc_file}" "$@"
     '';
 in
 
