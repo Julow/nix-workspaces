096c015f17e680292b92087629b695e45045c2a8
Jules Aguillon <juloo.dsi@gmail.com>
2021-09-22 12:32:49 +0200

    Git: 'MAIN' symbolic-ref pointing to the main branch
    The 'git.main_branch' option is now used to define the MAIN symbolic
    ref, which can be used to write aliases that are agnostic to the actual
    name of the main branch. (eg. 'master', 'main', 'trunk')

--- modules/git.nix
+++ modules/git.nix
@@ -5,13 +5,24 @@ with lib;
 let
   conf = config.git;
 
+  # Sync remotes
   sync_remotes = ''
-    # Sync remotes
     ${pkgs.bash}/bin/bash ${./sync_git_remotes.sh} <<"EOF"
     ${concatStringsSep "\n" (mapAttrsToList (n: u: "${n} ${u}") conf.remotes)}
     EOF
   '';
 
+  # Sync the MAIN symbolic ref
+  # Also update the 'init.defaultBranch' config in case it's used by some scripts
+  sync_default_branch = ''
+    main=MAIN
+    def_branch=${escapeShellArg conf.main_branch}
+    main_ref=refs/heads/$def_branch
+    if [[ `git symbolic-ref "$main" 2>/dev/null` != $main_ref ]]; then
+      git symbolic-ref "$main" "$main_ref"
+      git config init.defaultBranch "$def_branch"
+    fi
+  '';
 in
 
 {
@@ -25,6 +36,11 @@ in
     main_branch = mkOption {
       type = types.str;
       default = "master";
+      description = ''
+        Defines the 'MAIN' symbolic ref.
+        Also used to set the 'init.defaultBranch' config and for the initial
+        checkout.
+      '';
     };
 
     main_remote = mkOption {
@@ -44,12 +60,14 @@ in
 
     init_script = ''
       git init
+      ${sync_default_branch}
       ${sync_remotes}
       git fetch --all
-      git checkout --track "${conf.main_remote}/${conf.main_branch}"
+      git checkout --track ${escapeShellArg "${conf.main_remote}/${conf.main_branch}"}
     '';
 
     activation_script = ''
+      ${sync_default_branch}
       ${sync_remotes}
 
       ${if config.git.gitignore == "" then "" else ''
